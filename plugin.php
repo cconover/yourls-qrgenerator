b488b64f43fa5541ea38157acbfd063b8d391d76
cconover <dev@cconover.com>
2014-03-10 21:01:57 +0000

    Switched plugin back to object-oriented syntax.

--- plugin.php
+++ plugin.php
@@ -8,32 +8,41 @@ Author: Christiaan Conover
 Author URI: https://christiaanconover.com/?ref=yourls-qrplugin-author
 */
 
-// Trigger the QR code generator if YOURLS doesn't recognize the URL pattern
-yourls_add_action( 'loader_failed', array( $this, 'generateqr' ) );
-
-// Do not allow a direct call to the plugin file
-if ( ! defined( 'YOURLS_ABSPATH' ) ) {
-	die();
-}
-
-// Require PHP QR Code library
-require_once( 'phpqrcode.php' );
-	
-function generateqr ( $request ) {
-	// Make Regex pattern for keyword
-	$pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
-	// Identify short URL using keyword pattern
-	if( preg_match( "@^([$pattern]+)\.qr?/?$@", $request[0], $matches ) ) {
-		// Validate that the keyword exists
-		$keyword = yourls_sanitize_keyword( $matches[1] );
-		// If the keyword exists, generate and display the QR code
-		if( yourls_is_shorturl( $keyword ) ) {
-			// URL to send to the QR code generator
-			$url = urlencode( YOURLS_SITE . '/' . $keyword . '?ref=qr' );
-			
-			// Generate and display QR code
-			QRCode::png( $url );
+// Plugin class
+class cconover_qrcode {
+	function __construct() {
+		// Trigger the QR code generator if YOURLS doesn't recognize the URL pattern
+		yourls_add_action( 'loader_failed', array( $this, 'generateqr' ) );
+		
+		// Do not allow a direct call to the plugin file
+		if ( ! defined( 'YOURLS_ABSPATH' ) ) {
+			die();
 		}
-	}
-} // End generateqr()
+		
+		// Require PHP QR Code library
+		require_once( dirname( __FILE__ ) . '/phpqrcode.php' );
+	} // End __construct()
+	
+	public function generateqr ( $request ) {
+		// Make Regex pattern for keyword
+		$pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
+
+		// Identify short URL using keyword pattern
+		if( preg_match( "@^([$pattern]+)\.qr?/?$@", $request[0], $matches ) ) {
+			// Validate that the keyword exists
+			$keyword = yourls_sanitize_keyword( $matches[1] );
+			// If the keyword exists, generate and display the QR code
+			if( yourls_is_shorturl( $keyword ) ) {
+				// URL to send to the QR code generator
+				$url = urlencode( YOURLS_SITE . '/' . $keyword . '?ref=qr' );
+				
+				// Generate and display QR code
+				QRCode::png( $url );
+			}
+		}
+	} // End generateqr()
+} // End cconover_qrcode
+
+// Create new QR code object
+$qrcode = new cconover_qrcode;
 ?>
