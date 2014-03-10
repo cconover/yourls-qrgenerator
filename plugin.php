329f7924d5455a25a742f93cd1c0346a5b00b162
cconover <dev@cconover.com>
2014-03-10 20:29:34 +0000

    Plugin now uses PHP QR Code library instead of Google Chart API

--- plugin.php
+++ plugin.php
@@ -18,73 +18,44 @@ class cconover_qrcode {
 		if ( ! defined( 'YOURLS_ABSPATH' ) ) {
 			die();
 		}
+		
+		// Require PHP QR Code Class
+		require_once( 'phpqrcode.php' );
 	} // End __construct()
 	
 	public function generateqr ( $request ) {
-		// Check for cURL and GD on the server
-		if ( function_exists( 'curl_version' ) && function_exists ( 'imagecreatefromstring' ) ) {
-			// Make Regex pattern for keyword
-			$pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
-	
-			// If valid QR code size is provided, use it.  If not, default to 200px square
-			if ( isset( $_GET['s'] ) && is_numeric( $_GET['s'] ) && ( $_GET['s'] > 0 && $_GET['s'] <= 540 ) ) {
-				$size = $_GET['s'];
-			}
-			else {
-				$size = 200;
-			}
-	
-			// Set Error Correction Level
-			if ( isset($_GET['ecl'] ) && ( strtoupper( $_GET['ecl'] ) == 'L' || strtoupper( $_GET['ecl'] ) == 'M' || strtoupper( $_GET['ecl'] ) == 'Q' || strtoupper( $_GET['ecl'] ) == 'H' ) ) {
-				$ecl = strtoupper( $_GET['ecl'] );
-			}
-			else {
-				$ecl = 'M';
-			}
-	
-			// Identify short URL using keyword pattern
-			if( preg_match( "@^([$pattern]+)\.qr?/?$@", $request[0], $matches ) ) {
-				// Validate that the keyword exists
-				$keyword = yourls_sanitize_keyword( $matches[1] );
-				// If the keyword exists, generate and display the QR code
-				if( yourls_is_shorturl( $keyword ) ) {
-					// URL to send to the QR code generator
-					$url = urlencode( YOURLS_SITE . '/' . $keyword . '?ref=qr' );
-					// Google QR Code API URL
-					$generateurl = 'https://chart.googleapis.com/chart?chs=' . $size . 'x' . $size . '&cht=qr&chld=' . $ecl . '&chl=' . $url;
-				
-					// Initialize cURL
-					$ch = curl_init( $generateurl );
-					curl_setopt( $ch, CURLOPT_HEADER, 0 );
-					curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-					curl_setopt( $ch, CURLOPT_BINARYTRANSFER,1 );
-					curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-				
-					// Fetch QR code and close cURL session
-					$qrdata = curl_exec( $ch );
-					curl_close( $ch );
-					
-					// Generate image from data retrived from Google API
-					$qrimg = imagecreatefromstring( $qrdata );
-					$result = $qrimg;
-					imagedestroy( $qrimg );
-				}
-			}
-		}
-		// If cURL is installed, but not GD
-		elseif ( function_exists( 'curl_version' ) && ! function_exists( 'imagecreatefromstring' ) ) {
-			$result = 'Your server does not have GD installed, which is required to use this plugin.';
-		}
-		// If GD is installed, but not cURL
-		elseif ( function_exists( 'imagecreatefromstring' ) && ! function_exists( 'curl_version' ) ) {
-			$result = 'Your server does not have cURL installed, which is required to use this plugin.';
+		// Make Regex pattern for keyword
+		$pattern = yourls_make_regexp_pattern( yourls_get_shorturl_charset() );
+
+		// If valid QR code size is provided, use it.  If not, default to 200px square
+		if ( isset( $_GET['s'] ) && is_numeric( $_GET['s'] ) && ( $_GET['s'] > 0 && $_GET['s'] <= 540 ) ) {
+			$size = $_GET['s'];
 		}
 		else {
-			$result = 'Your server does not have cURL or GD installed, both of which are required to use this plugin.';
+			$size = 200;
 		}
-		
-		// Display the result
-		echo $result;
+
+		// Set Error Correction Level
+		if ( isset($_GET['ecl'] ) && ( strtoupper( $_GET['ecl'] ) == 'L' || strtoupper( $_GET['ecl'] ) == 'M' || strtoupper( $_GET['ecl'] ) == 'Q' || strtoupper( $_GET['ecl'] ) == 'H' ) ) {
+			$ecl = strtoupper( $_GET['ecl'] );
+		}
+		else {
+			$ecl = 'M';
+		}
+
+		// Identify short URL using keyword pattern
+		if( preg_match( "@^([$pattern]+)\.qr?/?$@", $request[0], $matches ) ) {
+			// Validate that the keyword exists
+			$keyword = yourls_sanitize_keyword( $matches[1] );
+			// If the keyword exists, generate and display the QR code
+			if( yourls_is_shorturl( $keyword ) ) {
+				// URL to send to the QR code generator
+				$url = urlencode( YOURLS_SITE . '/' . $keyword . '?ref=qr' );
+				
+				// Generate and display QR code
+				QRCode::png( $url );
+			}
+		}
 	} // End generateqr()
 } // End cconover_qrcode
 
